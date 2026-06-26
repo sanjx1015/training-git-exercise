94e5e426548fc147a6293db91e6ddd68bd5bfba0
sanjx1015 <sanjx1015@gmail.com>
2026-06-25 20:56:01 -0600

    Add trivia batch script

--- src/setup.bat
+++ src/setup.bat
@@ -2,7 +2,11 @@
 
 mkdir deployment
 
+<<<<<<< HEAD
 copy docs\README.md deploymente\README.md
+=======
+copy docs\README.md deployment\README.md
+>>>>>>> fd41bc9ba286533e3173e09afca838ed3feb9d30
 
 echo Setup complete
 
